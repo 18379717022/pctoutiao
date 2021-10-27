96ca415e301a135d13869e4da7e41b045abd05e7
18379717022 <1510783790@qq.com>
2021-10-27 23:12:06 +0800

    提交pctoutiao到本地仓库

---
+++ pcelementdemo/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './plugins/element.js'
+import './assets/css/base.css'
+import './assets/ali/iconfont.css'
+import TreeTable from 'vue-table-with-tree-grid'
+
+Vue.config.productionTip = false
+
+Vue.component('tree-table',TreeTable)
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
